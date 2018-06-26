87d9027326f13a1793169b3fb058721608380e01
LLevella <38900844+LLevella@users.noreply.github.com>
2018-06-27 00:50:29 +0300

    Update 1-9.py
    Убран глобальный  cook_book

--- V1-9/1-9.py
+++ V1-9/1-9.py
@@ -1,25 +1,5 @@
-cook_book = {
-  'яйчница': [
-    {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-    ],
-  'стейк': [
-    {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-    {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-    {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
-    ],
-  'салат': [
-    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-    {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
-    {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-    {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-    ]
-  }
 
-
-
-
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
   shop_list = {}
   for dish in dishes:
     for ingridient in cook_book[dish]:
@@ -37,11 +17,11 @@ def print_shop_list(shop_list):
     print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
                             shop_list_item['measure']))
 
-def create_shop_list():
+def create_shop_list(cook_book):
   person_count = int(input('Введите количество человек: '))
   dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
     .lower().split(', ')
-  shop_list = get_shop_list_by_dishes(dishes, person_count)
+  shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
   print_shop_list(shop_list)
 
 
@@ -60,20 +40,38 @@ def create_ing_list(f, n_ing):
         # create_dict_from_list(one_of_ingridients)
     return ingridients
 
-def input_data():
+def input_data(cook_book):
     with open('input.txt', 'r') as fin:
         for fline in fin:
             name = fline.strip().lower()
             n_ing = int(fin.readline().strip())
             cook_book[name] = create_ing_list(fin, n_ing)
             fin.readline()
+     return cook_book
 
 
 
 def main():
-    input_data()
+  cook_book = {
+  'яйчница': [
+    {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
+    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
+    ],
+  'стейк': [
+    {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
+    {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
+    {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
+    ],
+  'салат': [
+    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
+    {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
+    {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
+    {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
+    ]
+  }
+    cook_book = input_data(cook_book)
     print(cook_book)
-    create_shop_list()
+    create_shop_list(cook_book)
 
 
 
